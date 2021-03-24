1504ec89ada4e4dcd5f8217b8f74db209d4eee35
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2021-03-24 19:20:17 -0300

    Struct Project

---
+++ TwitterSwiftUI/View/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUI
+//
+//  Created by Brendon Bitencourt Braga on 2021-03-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                FeedView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
+            
+            // NavigationView Modifiers
+            .navigationBarTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 12 Pro")
+    }
+}
