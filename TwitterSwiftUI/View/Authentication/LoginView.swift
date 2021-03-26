9989ef65c045a5863034dc904b437705f4cca366
Brendon Bitencourt Braga <brendon.braga@gmail.com>
2021-03-26 18:17:04 -0300

    ImagePicker

--- TwitterSwiftUI/View/Authentication/LoginView.swift
+++ TwitterSwiftUI/View/Authentication/LoginView.swift
@@ -13,68 +13,74 @@ struct LoginView: View {
     @State private var password: String = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("twitter-logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 220, height: 100)
-                    .padding(.top, 88)
-                    .padding(.bottom)
-                
+        NavigationView {
+            ZStack {
                 VStack {
-                    CustomTextField("Email", text: $email, imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
+                    Image("twitter-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 220, height: 100)
+                        .padding(.top, 88)
+                        .padding(.bottom)
                     
-                    CustomSecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                }
-                .padding(.vertical)
-                
-                HStack {
+                    VStack {
+                        CustomTextField("Email", text: $email, imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                        
+                        CustomSecureField("Password", text: $password)
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.vertical)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {}, label: {
+                            Text("Forgot password?")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                                .bold()
+                        })
+                    }
+                    
+                    Button(action: {}, label: {
+                        Text("Sign in")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .frame(width: 360, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                    })
+                    .padding(.top)
+                    
                     Spacer()
                     
-                    Button(action: {}, label: {
-                        Text("Forgot password?")
-                            .font(.footnote)
+                    NavigationLink(
+                        destination: RegistrationView(),
+                        label: {
+                            HStack {
+                                Text("Don't have an account?")
+                                
+                                Text("Sign up")
+                                    .bold()
+                            }
+                            .padding(.bottom, 32)
                             .foregroundColor(.white)
-                            .bold()
-                    })
-                }
-                
-                Button(action: {}, label: {
-                    Text("Sign in")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 360, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                })
-                .padding(.top)
-                
-                Spacer()
-                
-                HStack {
-                    Text("Don't have an account?")
+                        })
                     
-                    Button(action: {}, label: {
-                        Text("Sign up")
-                            .bold()
-                    })
                 }
-                .padding(.bottom, 32)
-                .foregroundColor(.white)
+                .padding(.horizontal, 32)
             }
-            .padding(.horizontal, 32)
+            .background(Color(#colorLiteral(red: 0.1184234694, green: 0.6316044927, blue: 0.951161921, alpha: 1)))
+            .ignoresSafeArea()
         }
-        .background(Color(#colorLiteral(red: 0.1184234694, green: 0.6316044927, blue: 0.951161921, alpha: 1)))
-        .ignoresSafeArea()
+        .accentColor(.white)
     }
 }
 
--- TwitterSwiftUI/View/Authentication/RegistrationView.swift
+++ TwitterSwiftUI/View/Authentication/RegistrationView.swift
@@ -8,8 +8,89 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var fullName: String = ""
+    @State private var userName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    @State private var showImagePicker = false
+    @State private var selectedUIImage: UIImage?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Button(action: {
+                    showImagePicker.toggle()
+                }, label: {
+                    if let image = selectedUIImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 140, height: 140)
+                            .clipped()
+                            .clipShape(Circle())
+                    } else {
+                        Image("plus_photo")
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(.white)
+                            .scaledToFill()
+                            .frame(width: 140, height: 140)
+                            
+                    }
+                })
+                .padding(.top, 88)
+                .padding(.bottom)
+                .sheet(isPresented: $showImagePicker) {
+                    ImagePicker(sourceType: .photoLibrary) { (image) in
+                        self.selectedUIImage = image
+                    }
+                }
+                
+                VStack {
+                    CustomTextField("Full Name", text: $fullName, imageName: "person")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField("Username", text: $userName, imageName: "person.fill")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField("Email", text: $email, imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical)
+                
+                Button(action: {}, label: {
+                    Text("Sign up")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                })
+                .padding(.top)
+                
+                Spacer()
+            }
+            .padding(.horizontal, 32)
+        }
+        .background(Color(#colorLiteral(red: 0.1184234694, green: 0.6316044927, blue: 0.951161921, alpha: 1)))
+        .ignoresSafeArea()
     }
 }
 
